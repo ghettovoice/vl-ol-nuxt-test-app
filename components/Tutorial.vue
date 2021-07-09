bd14221702f766f4d29dcbad43cfee01e76b1725
Vladimir Vershinin <ghettovoice@gmail.com>
2021-07-09 14:09:39 +0300

    Setup openlayers & vuelayers as nuxt plugins

---
+++ components/Tutorial.vue
@@ -0,0 +1,21 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <client-only>
+      <vl-map style="height: 500px">
+        <vl-layer-tile>
+          <vl-source-osm />
+        </vl-layer-tile>
+      </vl-map>
+    </client-only>
+  </div>
+</template>
+
+<script>
+if (process.client) {
+  console.dir(require('~/plugins/ol'))
+}
+
+export default {
+}
+</script>
